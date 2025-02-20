90c73f5e8a637470bbd2e5fb7be125343d95cf40
nguyenngocdue <duengocnguyen@gmail.com>
2025-02-20 14:22:31 +0700

    minor

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -11,6 +11,7 @@ export default function UserForm() {
 
   const onSubmit = async (data: UserFormData) => {
     try {
+        console.log(`${import.meta.env.VITE_API_BASE_URL}/users`);
       const response = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/users`, data);
       console.log('User created:', response.data);
       alert('User created successfully!');
