1566cab6afe231c3098bcdf29e838d731534ccb6
nguyenngocdue <duengocnguyen@gmail.com>
2025-03-18 22:39:44 +0700

    minor

--- scripts/deploy_beta.sh
+++ scripts/deploy_beta.sh
@@ -2,5 +2,6 @@
 
 echo "⚙️ Deploy Beta..."
 docker-compose -f .docker_202/docker-compose202.yml down --remove-orphans 
-docker-compose -f .docker_202/docker-compose202.yml up -d --build
+# docker-compose -f .docker_202/docker-compose202.yml up -d --build
+docker-compose -f .docker_202/docker-compose202.yml up -d 
 echo "✅ Beta Deployed!"
