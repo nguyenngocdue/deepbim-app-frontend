8bd1d3e843f8b0d14b957324c84fff36378b865a
nguyenngocdue <duengocnguyen@gmail.com>
2025-03-19 08:10:01 +0700

    minor

--- scripts/deploy_beta.sh
+++ scripts/deploy_beta.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "⚙️ Deploy Beta..."
+git pull
 docker-compose -f .docker_202/docker-compose202.yml down --remove-orphans 
 # docker-compose -f .docker_202/docker-compose202.yml up -d --build
 docker-compose -f .docker_202/docker-compose202.yml up -d 
