14df563ac1a6f80be46f750f384d7c7c4b27c9bf
nguyenngocdue <duengocnguyen@gmail.com>
2025-03-19 08:25:43 +0700

    minor

--- scripts/deploy_beta.sh
+++ scripts/deploy_beta.sh
@@ -6,3 +6,6 @@ docker-compose -f .docker_202/docker-compose202.yml down --remove-orphans
 # docker-compose -f .docker_202/docker-compose202.yml up -d --build
 docker-compose -f .docker_202/docker-compose202.yml up -d 
 echo "✅ Beta Deployed!"
+
+echo "✅ Show Dockers are running!"
+docker ps 
