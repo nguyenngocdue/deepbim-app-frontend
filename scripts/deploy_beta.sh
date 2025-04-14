39a41b1ff44f54ceff03f438d08e2243f0384e2c
nguyenngocdue <duengocnguyen@gmail.com>
2025-04-14 09:54:29 +0700

    minor

--- scripts/deploy_beta.sh
+++ scripts/deploy_beta.sh
@@ -3,8 +3,11 @@ echo "Pull Code from GitHub..."
 git pull
 echo "⚙️ Deploy Beta..."
 docker-compose -f .docker_202/docker-compose202.yml down --remove-orphans 
-docker-compose -f .docker_202/docker-compose202.yml up -d --build
+# docker-compose -f .docker_202/docker-compose202.yml up -d --build
 # docker-compose -f .docker_202/docker-compose202.yml up -d 
+# docker-compose -f .docker_202/docker-compose202.yml build 
+docker-compose -f .docker_202/docker-compose202.yml restart
+
 echo "✅ Beta Deployed!"
 
 echo "✅ Show Dockers are running!"
