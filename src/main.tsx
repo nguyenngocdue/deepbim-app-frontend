dc2939ecab7ee9c36c3faa1a531911429bd1d099
nguyenngocdue <duengocnguyen@gmail.com>
2025-04-15 13:32:07 +0700

    minor

--- src/main.tsx
+++ src/main.tsx
@@ -86,6 +86,7 @@ const router = createRouter({
   defaultPreloadStaleTime: 0,
 })
 
+
 // Register the router instance for type safety
 declare module '@tanstack/react-router' {
   interface Register {
