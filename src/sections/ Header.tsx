c8a82141ac262f9e9b7cacbc615f94d24ff45966
nguyenngocdue <duengocnguyen@gmail.com>
2025-04-16 03:36:17 +0700

    add images

--- src/sections/ Header.tsx
+++ src/sections/ Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="flex justify-between items-center px-10 py-1">
         {/* Logo */}
         <div className="flex text-center items-center">
-          <img src="/logo/normal-logo.png" className="h-12 w-12" alt="Logo" />
+          <img src="/logo/logo.png" className="h-12 w-12" alt="Logo" />
           <h1 className="text-xl font-bold text-green-600">DeepBIM</h1>
         </div>
 
