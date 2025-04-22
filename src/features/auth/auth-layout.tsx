bb9c91b3b3c97d2a8fa0f8cc16fec5d062b49cdc
nguyenngocdue <duengocnguyen@gmail.com>
2025-04-22 23:00:04 +0700

    update forget password

--- src/features/auth/auth-layout.tsx
+++ src/features/auth/auth-layout.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function AuthLayout({ children }: Props) {
   return (
-    <div className='bg-page-50 container grid h-svh overflow-y-auto flex-col items-center justify-center lg:max-w-none lg:px-0'>
+    <div className='bg-behind container grid h-svh overflow-y-auto flex-col items-center justify-center lg:max-w-none lg:px-0'>
       <div className='mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[480px] lg:p-8'>
         <div className='m-auto p-2'>
           <LogoWord />
