85850460869e380c1cbc022d65db60026658b8c6
nguyenngocdue <duengocnguyen@gmail.com>
2025-04-23 17:16:55 +0700

    add ParticlesContainer to sign in

--- src/features/auth/sign-in/sign-in-2.tsx
+++ src/features/auth/sign-in/sign-in-2.tsx
@@ -1,57 +1,62 @@
 import ViteLogo from '@/assets/vite.svg'
 import { UserAuthForm } from './components/user-auth-form'
 import { LogoWord } from '@/components/LogoWord'
+import ParticlesContainer from '@/components/ParticlesContainer'
 
 export default function SignIn2() {
   return (
-    <div className='  relative grid h-svh flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0 w-full  bg-behind'>
-      <div className='relative hidden h-full flex-col bg-muted p-10 text-white lg:flex'>
-        {/* Thay đổi gradient từ emerald sang blue/indigo */}
-        <div className="absolute inset-0 bg-gradient-to-br from-[#0f172a] via-[#1e293b] to-[#334155]" />
-        <div className='relative z-20 flex items-center text-lg font-medium'>
-          <LogoWord/>
+    <div className='relative z-10'>
+      <ParticlesContainer/>
+      <div className='relative grid h-svh flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0 w-full'>
+        <div className='relative hidden h-full flex-col bg-muted p-10 text-white lg:flex bg-transparent'>
+          {/* Thay đổi gradient từ emerald sang blue/indigo */}
+          <div className="absolute  " />
+          <div className='relative z-20 flex items-center text-lg font-medium'>
+            <LogoWord/>
+          </div>
+          <img
+            src={ViteLogo}
+            className='relative m-auto'
+            width={301}
+            height={60}
+            alt='Vite'
+          />
+          <div className='relative z-20 mt-auto'>
+            <blockquote className='space-y-2'>
+              <p className='text-sm font-heading text-subtitle2'>
+                "Deep Bim is a powerful tool for BIM professionals, providing
+                advanced features and seamless integration with industry standards.
+                With its intuitive interface and robust functionality, it streamlines
+                workflows and enhances collaboration."
+              </p>
+              <footer className='text-sm'>Nissan</footer>
+            </blockquote>
+          </div>
         </div>
-        <img
-          src={ViteLogo}
-          className='relative m-auto'
-          width={301}
-          height={60}
-          alt='Vite'
-        />
-        <div className='relative z-20 mt-auto'>
-          <blockquote className='space-y-2'>
-            <p className='text-lg'>
-              "Deep Bim is a powerful tool for BIM professionals, providing
-              advanced features and seamless integration with industry standards.
-              With its intuitive interface and robust functionality, it streamlines
-              workflows and enhances collaboration."
+        <div className='lg:p-8  h-full bg-transparent'>
+          <div className='mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[500px]'>
+            <UserAuthForm />
+            <p className='px-8 text-center text-sm text-muted-foreground'>
+              By clicking login, you agree to our{' '}
+              <a
+                href='/terms'
+                className='underline underline-offset-4 hover:text-primary'
+              >
+                Terms of Service
+              </a>{' '}
+              and{' '}
+              <a
+                href='/privacy'
+                className='underline underline-offset-4 hover:text-primary'
+              >
+                Privacy Policy
+              </a>
+              .
             </p>
-            <footer className='text-sm'>Nissan</footer>
-          </blockquote>
+          </div>
         </div>
       </div>
-      <div className='lg:p-8  h-full bg-behind'>
-        <div className='mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[500px]'>
-          <UserAuthForm />
-          <p className='px-8 text-center text-sm text-muted-foreground'>
-            By clicking login, you agree to our{' '}
-            <a
-              href='/terms'
-              className='underline underline-offset-4 hover:text-primary'
-            >
-              Terms of Service
-            </a>{' '}
-            and{' '}
-            <a
-              href='/privacy'
-              className='underline underline-offset-4 hover:text-primary'
-            >
-              Privacy Policy
-            </a>
-            .
-          </p>
-        </div>
-      </div>
+
     </div>
   )
 }
