3a0052eb71dc707d0cf786c815ffe701aaf8d9f1
nguyenngocdue <duengocnguyen@gmail.com>
2025-04-23 11:50:20 +0700

    minor

--- src/features/auth/sign-in/sign-in-2.tsx
+++ src/features/auth/sign-in/sign-in-2.tsx
@@ -4,7 +4,7 @@ import { LogoWord } from '@/components/LogoWord'
 
 export default function SignIn2() {
   return (
-    <div className=' container relative grid h-svh flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0 w-full  bg-behind'>
+    <div className='  relative grid h-svh flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0 w-full  bg-behind'>
       <div className='relative hidden h-full flex-col bg-muted p-10 text-white lg:flex'>
         {/* Thay đổi gradient từ emerald sang blue/indigo */}
         <div className="absolute inset-0 bg-gradient-to-br from-[#0f172a] via-[#1e293b] to-[#334155]" />
@@ -31,7 +31,7 @@ export default function SignIn2() {
         </div>
       </div>
       <div className='lg:p-8  h-full bg-behind'>
-        <div className='mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[350px]'>
+        <div className='mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[500px]'>
           <UserAuthForm />
           <p className='px-8 text-center text-sm text-muted-foreground'>
             By clicking login, you agree to our{' '}
