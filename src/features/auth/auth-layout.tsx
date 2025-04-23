bdc491d47dd76866bb293a6a5814897d9ded36e9
nguyenngocdue <duengocnguyen@gmail.com>
2025-04-23 17:22:39 +0700

    add ParticlesContainer to sign up

--- src/features/auth/auth-layout.tsx
+++ src/features/auth/auth-layout.tsx
@@ -6,8 +6,8 @@ interface Props {
 
 export default function AuthLayout({ children }: Props) {
   return (
-    <div className='bg-behind container grid h-svh overflow-y-auto flex-col items-center justify-center lg:max-w-none lg:px-0'>
-      <div className='mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[480px] lg:p-8'>
+    <div className='container grid h-svh overflow-y-auto flex-col items-center justify-center lg:max-w-none lg:px-0'>
+      <div className='mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[480px] lg:p-8 bg-transparent'>
         <div className='m-auto p-2'>
           <LogoWord />
         </div>
--- src/features/auth/sign-up/index.tsx
+++ src/features/auth/sign-up/index.tsx
@@ -3,46 +3,50 @@ import { Card } from '@/components/ui/card'
 import AuthLayout from '../auth-layout'
 import { SignUpForm } from './components/sign-up-form'
 import { Separator } from '@/components/ui/separator'
+import ParticlesContainer from '@/components/ParticlesContainer'
 
 export default function SignUp() {
   return (
-    <AuthLayout>
-      <Card className='p-6 h-full bg-page-50'>
-        <div className='mb-2 flex flex-col space-y-2 text-left'>
-          <h1 className='text-lg font-semibold text-info tracking-tight text-white'>
-            Create an account
-          </h1>
-          <p className='text-sm text-muted-foreground'>
-            Enter your email and password to create an account. <br />
-            Already have an account?{' '}
-            <Link
-              to='/sign-in'
-              className='font-medium text-blue-400 hover:text-blue-300 transition-colors duration-200'
+    <div className='relative z-10'>
+      <ParticlesContainer/>
+      <AuthLayout>
+        <Card className='p-6 h-full '>
+          <div className='mb-2 flex flex-col space-y-2 text-left'>
+            <h1 className='text-lg font-semibold text-info tracking-tight text-white'>
+              Create an account
+            </h1>
+            <p className='text-sm text-muted-foreground'>
+              Enter your email and password to create an account. <br />
+              Already have an account?{' '}
+              <Link
+                to='/sign-in'
+                className='font-medium text-blue-400 hover:text-blue-300 transition-colors duration-200'
+              >
+                Sign In
+              </Link>
+            </p>
+          </div>
+          <Separator orientation='horizontal' className='bg-zinc-500'/>
+          <SignUpForm />
+          <p className='mt-4 px-8 text-center text-sm text-muted-foreground'>
+            By creating an account, you agree to our{' '}
+            <a
+              href='/terms'
+              className='underline underline-offset-4 hover:text-primary'
             >
-              Sign In
-            </Link>
+              Terms of Service
+            </a>{' '}
+            and{' '}
+            <a
+              href='/privacy'
+              className='underline underline-offset-4 hover:text-primary'
+            >
+              Privacy Policy
+            </a>
+            .
           </p>
-        </div>
-        <Separator orientation='horizontal' className='bg-zinc-500'/>
-        <SignUpForm />
-        <p className='mt-4 px-8 text-center text-sm text-muted-foreground'>
-          By creating an account, you agree to our{' '}
-          <a
-            href='/terms'
-            className='underline underline-offset-4 hover:text-primary'
-          >
-            Terms of Service
-          </a>{' '}
-          and{' '}
-          <a
-            href='/privacy'
-            className='underline underline-offset-4 hover:text-primary'
-          >
-            Privacy Policy
-          </a>
-          .
-        </p>
-      </Card>
-    </AuthLayout>
+        </Card>
+      </AuthLayout>
+    </div>
   )
 }
