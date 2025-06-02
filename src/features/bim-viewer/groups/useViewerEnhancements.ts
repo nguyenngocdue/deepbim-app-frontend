eec11be647846ee51f3474f1de6de40f8b32fba7
nguyenngocdue <duengocnguyen@gmail.com>
2025-06-02 16:16:54 +0700

    fix: npm install

--- src/features/bim-viewer/groups/useViewerEnhancements.ts
+++ src/features/bim-viewer/groups/useViewerEnhancements.ts
@@ -5,20 +5,74 @@ import { useClippingEdges } from "../useClippingEdges";
 import { useGrids } from "../useGrid";
 import { useCameraType } from "../useCameraType";
 
+interface FeatureFlags {
+  haveGrids: boolean;
+  havePlansViews: boolean;
+  haveWorldSettings: boolean;
+  isClippingEdges: boolean;
+  isOrthoPerspective: boolean;
+}
+
+interface UseViewerEnhancementsProps {
+  componentRef: React.RefObject<any>;
+  worldRef: React.RefObject<any>;
+  ifcContainerRef: React.RefObject<HTMLDivElement | null>;
+  modelRef: React.RefObject<any>;
+  featureFlags: FeatureFlags;
+}
+
+/**
+ * Hook bật các tính năng viewer: grid, camera, clipping, v.v.
+ */
 export function useViewerEnhancements({
   componentRef,
   worldRef,
   ifcContainerRef,
   modelRef,
   featureFlags,
-}: any) {
-  const { haveGrids, havePlansViews, haveWorldSettings, isClippingEdges, isOrthoPerspective} = featureFlags;
+}: UseViewerEnhancementsProps) {
+  const {
+    haveGrids,
+    havePlansViews,
+    haveWorldSettings,
+    isClippingEdges,
+    isOrthoPerspective,
+  } = featureFlags;
 
   useEffect(() => {
     useGrids({ haveGrids });
-    usePlaneViews({ havePlansViews, componentRef, worldRef, ifcContainerRef, modelRef });
-    useWorldSettings({ haveWorldSettings, componentRef, worldRef });
-    useClippingEdges({isClippingEdges , componentRef, worldRef, ifcContainerRef });
-    useCameraType({isOrthoPerspective});
-  }, [ haveGrids, havePlansViews, haveWorldSettings, isClippingEdges, isOrthoPerspective]);
+
+    usePlaneViews({
+      havePlansViews,
+      componentRef,
+      worldRef,
+      ifcContainerRef,
+      modelRef,
+    });
+
+    useWorldSettings({
+      haveWorldSettings,
+      componentRef,
+      worldRef,
+    });
+
+    useClippingEdges({
+      isClippingEdges,
+      componentRef,
+      worldRef,
+      ifcContainerRef,
+    });
+
+    useCameraType({ isOrthoPerspective });
+  }, [
+    haveGrids,
+    havePlansViews,
+    haveWorldSettings,
+    isClippingEdges,
+    isOrthoPerspective,
+    componentRef,
+    worldRef,
+    ifcContainerRef,
+    modelRef,
+  ]);
 }
