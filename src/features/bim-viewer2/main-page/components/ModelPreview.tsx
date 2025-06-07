dd2a847a340ae66b3263bf5815410289e56f3c52
nguyenngocdue <duengocnguyen@gmail.com>
2025-06-07 11:33:54 +0700

    minor

--- src/features/bim-viewer2/main-page/components/ModelPreview.tsx
+++ src/features/bim-viewer2/main-page/components/ModelPreview.tsx
@@ -8,55 +8,55 @@ const mockModels = [
     title: "Factory Model",
     description: "An industrial building exported from Tekla in IFC format. You can explore both simple and complex structural components in a fully interactive 3D environment.",
     imageUrl: "/assets/viewers/factory-model.png",
-    linkUrl: "/view2/factory-tekla"
+    linkUrl: "/view?v=d4f71669b6f27e5729a463e1"
   },
   {
     title: "Steel Frame Warehouse",
     description: "A detailed structural model of a steel-frame warehouse exported in IFC format. Explore beams, columns, and floor slabs in a clean 3D environment designed for structural analysis and coordination.",
     imageUrl: "/assets/viewers/steel-frame-warehouse-ifc.png",
-    linkUrl: "/view2/factory-tekla"
+    linkUrl: "/view?v=d618e000cae9d9d4b7dead74"
   },
   {
     title: "Civil 3D Road Design",
     description: "A roadway corridor model designed in Civil 3D, including alignment, profile, assemblies, and terrain surface. Visualize the full geometry of the road within a 3D context for planning and analysis.",
     imageUrl: "/assets/viewers/civil3d-road.png",
-    linkUrl: "/view2/civil3d-road"
+    linkUrl: "/view?v=0e913a2526acecbfe0342202"
   },
   {
     title: "Multi-story Structural Model",
     description: "A detailed multi-floor concrete structure featuring columns, slabs, and beams. Designed for structural analysis and coordination, this BIM model visualizes the building frame layout and core components clearly in 3D.",
     imageUrl: "/assets/viewers/multistory-structure.png",
-    linkUrl: "/view2/multistory-structure"
+    linkUrl: "/view?v=63befdd0c3a8a6338cb33040"
   },
   {
     "title": "",
     "description": "",
     "imageUrl": "/assets/viewers/cantilever-bridge-girder.png",
-    "linkUrl": "/view2/cantilever-bridge-girder"
+    "linkUrl": "/view?v=726eff8867fc882115793981"
   },
   {
-    "title": "MEP Coordination Model",
+    "title": "MEP Coordination Model (500MB)",
     "description": "A comprehensive 3D BIM model showcasing MEP systems including HVAC ducting, piping, and electrical routing. Ideal for clash detection, spatial planning, and interdisciplinary coordination in building design.",
     "imageUrl": "/assets/viewers/mep-coordination.png",
-    "linkUrl": "/view2/mep-coordination"
+    "linkUrl": "/view?v=96b0bedd366f2c4128ad28ed"
   },
   {
     "title": "",
     "description": "",
     "imageUrl": "/assets/viewers/bridge-pier-rebar.png",
-    "linkUrl": "/view2/view?v=e1441b222bdbdc8d11a7844c" //Bridge_Pier_R25.ifc
+    "linkUrl": "/view?v=00eddf035702c488b7448d52" //Bridge_Pier_R25.ifc
   },
   {
     "title": "Mountain Tunnel Excavation",
     "description": "3D model of a mountain tunnel segment in Japan, showcasing excavation geometry and structural lining for underground infrastructure design and construction analysis.",
     "imageUrl": "/assets/viewers/mountain-tunnel-japan.jpg",
-    "linkUrl": "/view2/mountain-tunnel-japan" //
+    "linkUrl": "/view?v=1b31bc1c4c12006ab8dadb08" //
   },
   {
     "title": "Simple Bridge Model",
     "description": "3D visualization of a simply supported bridge structure, illustrating pier cap, girders, and support detailing. Ideal for structural planning, analysis, and bridge design validation.",
     "imageUrl": "/assets/viewers/simply_supported_bridge.png",
-    "linkUrl": "/view2/simply_supported_bridge" //K2.ifc
+    "linkUrl": "/view?v=e8ed97dfbf9527fdac576909" //K2.ifc
   }
 
 ]
