2436b1cedb2a9c1f4466a96627b88f8059d256b3
nguyenngocdue <duengocnguyen@gmail.com>
2025-06-10 10:53:46 +0700

    fix:  push code

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -2,14 +2,14 @@
 echo "Pull Code from GitHub..."
 git pull
 echo "⚙️ Deploy Production..."
-# docker-compose -f .docker_102/docker-compose102.yml down --remove-orphans 
-# docker-compose -f .docker_102/docker-compose102.yml up -d --build 
+docker-compose -f .docker_102/docker-compose102.yml down --remove-orphans 
+docker-compose -f .docker_102/docker-compose102.yml up -d --build 
 
-docker-compose -f .docker_102/docker-compose102.yml down down --volumes --remove-orphans
-docker system prune -f --volumes
-docker-compose -f .docker_102/docker-compose102.yml up --build --no-cache
-docker-compose -f .docker_102/docker-compose102.yml up up -d
-echo "✅ Beta Deployed!"
+# docker-compose -f .docker_102/docker-compose102.yml down --volumes --remove-orphans
+# docker system prune -f --volumes
+# docker-compose -f .docker_102/docker-compose102.yml up --build --no-cache
+# docker-compose -f .docker_102/docker-compose102.yml up up -d
+echo "✅ Production Deployed!"
 
 echo "✅ Show Dockers are running!"
 docker ps 
