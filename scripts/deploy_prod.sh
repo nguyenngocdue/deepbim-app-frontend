477e786843a011c4e7118410c924ee19e308c24b
nguyenngocdue <duengocnguyen@gmail.com>
2025-06-10 10:41:41 +0700

    fix:  push code

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -3,7 +3,7 @@ echo "Pull Code from GitHub..."
 git pull
 echo "⚙️ Deploy Production..."
 docker-compose -f .docker_102/docker-compose102.yml down --remove-orphans 
-docker-compose -f .docker_102/docker-compose102.yml up -d --build --no-cache
+docker-compose -f .docker_102/docker-compose102.yml up -d --build 
 echo "✅ Beta Deployed!"
 
 echo "✅ Show Dockers are running!"
